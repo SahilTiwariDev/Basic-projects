d1d881e3921ead7a902b8fbd3c10d11ef8169323
SahilTiwariDev <sahiltiwari.contactme@gmail.com>
2025-03-09 23:47:56 +0530

    Create the task adding function

--- src/components/List_items.jsx
+++ src/components/List_items.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import unclicked from '../assets/unclicked_icon.png'
-import clicked from '../assets/clicked_icon.png'
-import deleteIcon from '../assets/delete_icon.png'
+import React from "react";
+import unclicked from "../assets/unclicked_icon.png";
+import clicked from "../assets/clicked_icon.png";
+import deleteIcon from "../assets/delete_icon.png";
 
-const List_items = () => {
+const List_items = ({text}) => {
   return (
-    <div className='flex w-2xl justify-between items-center'>
-        <div className='flex gap-2'>
-            <img className='h-7 w-7 cursor-pointer duration-200 active:scale-125 hover:scale-110' src={unclicked} alt="" />
-            <p className='text-lg'>Taks will be display here</p>
-        </div>
-        <div>
-            <img className='h-7 w-7 cursor-pointer duration-200 active:scale-125 hover:scale-110' src={deleteIcon} alt="" />
-        </div>
+    <div className="flex w-2xl justify-between items-center p-2">
+      <div className="flex gap-2">
+        <img
+          className="h-7 w-7 cursor-pointer duration-200 active:scale-125 hover:scale-110"
+          src={unclicked}
+          alt="unclicked_icon"
+        />
+        <p className="text-lg">{text}</p>
+      </div>
+      <div>
+        <img
+          className="h-7 w-7 cursor-pointer duration-200 active:scale-125 hover:scale-110"
+          src={deleteIcon}
+          alt="deleteIcon_icon"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default List_items
+export default List_items;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import List_items from "./List_items";
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  const inputRef = useRef();
+
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+    if (inputText === "") {
+      return null;
+    }
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isCompleted: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+
   return (
-    <div className="relative h-screen w-full flex items-center justify-center">
-      <div className="relative z-10 h-3/4 w-11/12 sm:w-1/2 flex flex-col items-center  bg-white rounded-lg shadow-lg p-5 gap-10">
-        <div className="flex items-center bg-gray-200 rounded-full h-12 w-2xl justify-between">
+    <div className="relative h-screen w-full flex flex-col items-center justify-center gap-5">
+      <div className="relative z-10 h-1/12 w-11/12 sm:w-1/2 flex flex-col items-center justify-center bg-white rounded-lg shadow-lg p-10">
+        <div className="flex items-center bg-gray-200 rounded-full h-12 w-full justify-between">
           <input
+            ref={inputRef}
             className="bg-transparent outline-none border-0 p-3 w-3/4 text-lg"
             type="text"
             placeholder="Enter new task"
           />
-          <button className="bg-blue-600 text-white text-lg h-12 rounded-full w-1/4 cursor-pointer duration-200 active:brightness-150 hover:brightness-110">
+          <button
+            onClick={add}
+            className="bg-blue-600 text-white text-lg h-12 rounded-full w-1/4 cursor-pointer duration-200 active:brightness-200 hover:brightness-110"
+          >
             Add
           </button>
         </div>
+      </div>
+      <div className="relative z-10 h-3/5 w-11/12 sm:w-1/2 flex flex-col items-center bg-white rounded-lg shadow-lg p-5 gap-10">
         <div>
-          <List_items />
+          {todoList.map((item, index) => {
+            return <List_items key={index} text={item.text} />;
+          })}
         </div>
       </div>
     </div>
